e522805f86a88878651fcbe692ac7d818f090120
Aditya Raj <adityaraj9983@gmail.com>
2023-04-01 23:13:39 +0530

    created cluster

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
 const connect = () =>
-  mongoose.connect(
-    "mongodb://127.0.0.1:27017",
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }
+  mongoose.connect(process.env.MONGO_URI,
+//      {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   }
   );
 
   module.exports = connect;
