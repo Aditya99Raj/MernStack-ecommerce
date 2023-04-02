04ccd7a8315235fd5e1c02923bef838f75048773
ashwanikpankaj <ashnit8294@gmail.com>
2023-04-02 13:15:29 +0530

    added-material-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -7,15 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. how are you feeeling
-          it guys
+          Edit <code>src/App.js</code> and save to reload. how are you feeeling it guys
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
