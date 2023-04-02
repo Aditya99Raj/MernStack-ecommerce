441a7d336ccde2dea643cd5880e7a983ac627044
Aditya Raj <adityaraj9983@gmail.com>
2023-04-02 11:51:51 +0530

    added error response

--- backend/src/routes/wishlist.routes.js
+++ backend/src/routes/wishlist.routes.js
@@ -2,28 +2,24 @@ const express = require("express");
 const wishlistModel = require("../models/wishlist.model");
 const router = express.Router();
 
-router.post("/wishlist",async(req,res)=>{
-    const {userId,category} = req.body;
-    try{
+router.post("/wishlist", async (req, res) => {
+  const { userId, category } = req.body;
+  try {
+    const wishlistItems = await wishlistModel.create(userId, category);
+    return res.status(201).send({ wishlistItems, response: "Success" });
+  } catch (err) {
+    return res.status(500).send({ err });
+  }
+});
 
-        const wishlistItems = await wishlistModel.create(userId,category)
-        return res.status(201).send({wishlistItems,response:'Success'})
-    }catch(err){
-        console.log(err)
-    }
-
-})
-
-router.get("/:userId",async(req,res)=>{
-    const {userId} = req.body;
-    try{
-        const wishlistItems = await wishlistModel.find(userId);
-        return res.status(201).send({wishlistItems})
-    }catch(err){
-        return res.status(500).send({err})
-    }
-})
-
-
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const wishlistItems = await wishlistModel.find(userId);
+    return res.status(201).send({ wishlistItems });
+  } catch (err) {
+    return res.status(500).send({ err });
+  }
+});
 
 module.exports = router;
